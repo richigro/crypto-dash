6add68368b4f74c16202a1419a4d3f3cbd019817
Ricardo Guerrero <richi_gro@hotmail.com>
2019-08-06 21:20:36 -0500

    added enzyme for component testing and boilerplate setupTests file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Welcome from './Welcome';
+
 export default function App() {
   return (
     <div>
       Hello SUcka!
+      <Welcome />
     </div>
   );
 };
