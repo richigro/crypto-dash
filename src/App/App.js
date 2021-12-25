789b22c0364345ab841e258e72740bcb36f4874a
Ricardo Guerrero <richi_gro@hotmail.com>
2021-12-24 18:49:31 -0600

    New Font

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import './App.css';
 import styled from 'styled-components';
-
+import './App.css'
 import Welcome from './Welcome';
 
-const MyButton = styled.div`
-  color: green;
-`;
 
 export default function App() {
   return (
     <div>
       <Welcome />
-      <MyButton>Hello</MyButton>
     </div>
   );
 };
