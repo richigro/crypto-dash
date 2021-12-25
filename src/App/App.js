878d3bf16c67e57d2562ffdc3fb40fbca62f9bf4
Ricardo Guerrero <richi_gro@hotmail.com>
2021-12-24 19:17:52 -0600

    very simple nav bar

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 import './App.css'
 
 import Welcome from './Welcome';
+import AppBar from './AppBar';
 import AppLayout from './AppLayout';
 
 
 export default function App() {
   return (
     <AppLayout>
+      <AppBar/>
       <Welcome />
     </AppLayout> 
   );
