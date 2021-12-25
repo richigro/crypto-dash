f4f6226dd239f6756bbf61003870278c906e9f28
Ricardo Guerrero <richi_gro@hotmail.com>
2021-12-24 19:04:57 -0600

    Add app layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import './App.css'
+
 import Welcome from './Welcome';
+import AppLayout from './AppLayout';
 
 
 export default function App() {
   return (
-    <div>
+    <AppLayout>
       <Welcome />
-    </div>
+    </AppLayout> 
   );
 };
