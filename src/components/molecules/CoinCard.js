5fed45834caface08d57c6497b1a25a36ff82120
ricardo guerrero <richi_gro@hotmail.com>
2022-01-08 14:56:26 -0800

    Different styles for cards

--- src/components/molecules/CoinCard.js
+++ src/components/molecules/CoinCard.js
@@ -8,12 +8,26 @@ const StyledCard = styled.div`
   background-color: white;
   border-radius: 4px;
   color: black;
-  padding: 2rem;
+  padding: 1.4rem;
+  padding-top: 0.6rem;
+  /* border: 5px solid pink; */
+  width: 10rem;
+  height: 10rem;
+  margin-right: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  border: 1px solid blue;
+  flex-grow: 1;
 `;
 
 const StyledCoinImage = styled.img`
   /* width: 3rem; */
   /* height: 3rem; */
+  /* flex-grow: 1; */
+  width: 2rem;
+  height: 2rem;
+  border: 5px solid red;
 `;
 
 const Flex = styled.div`
@@ -21,16 +35,36 @@ const Flex = styled.div`
 `;
 
 const CoinName = styled.div`
-  flex-grow: 1;
+  /* flex-grow: 1; */
+`;
+
+const CoinHeader = styled.div`
+  /* color: red; */
+  width: 100%;
+  padding: 0;
+  border: 1px solid orange;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledHr = styled.hr`
+  border: none;
+  border-top: 1px solid #aaa;
 `;
 
 function CoinCard({ coinData }) {
   const imageURL = `http://cryptocompare.com/${coinData.ImageUrl}`;
+  console.log("the coin data: ", coinData);
   return (
     <StyledCard>
-      <StyledCoinImage src={imageURL} alt={coinData.CoinName} />
-      <Flex>
+      <CoinHeader>
+        <ImageContainer>
+          <StyledCoinImage src={imageURL} alt={coinData.CoinName} />
+        </ImageContainer>
         <CoinName>{coinData.CoinName}</CoinName>
+      </CoinHeader>
+      <StyledHr />
+      <Flex>
         <div>{coinData.Symbol}</div>
       </Flex>
     </StyledCard>
